9326ede6e3d35bb14a3e00c4b57a4e8545536a2d
chaeseojin <chaeseojin267@gmail.com>
2024-09-12 16:30:09 +0900

    2주차 실습

---
+++ 2week/ex0.py
@@ -0,0 +1,12 @@
+# a = int(input("숫자를 입력하세요"))
+# b = a % 2
+
+# if b == 0:
+#    print('짝수이다 - True')
+# else :
+#    print('홀수이다 - False')
+
+num = int(input("숫자를 입력하세요"))
+# print(num)
+result = (num % 2) == 0
+print('짝수이다', result)
